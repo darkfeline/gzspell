a8ebb2617fc1390ccb0edc34384557c10b7ae635
Allen Li <darkfeline@abagofapples.com>
2013-09-09 20:44:10 -0400

    Split unit test assertion code into a method

--- gzspell/tests/test_trie.py
+++ gzspell/tests/test_trie.py
@@ -8,6 +8,10 @@ logger = logging.getLogger(__name__)
 
 class TestTrie(unittest.TestCase):
 
+    def check(self, t, err, comp):
+        self.assertIs(t.error, err)
+        self.assertIs(t.complete, comp)
+
     def test_one(self):
 
         x = trie.Trie()
@@ -16,23 +20,18 @@ class TestTrie(unittest.TestCase):
         logger.debug('test 1')
         t = trie.Traverser(x)
         t.traverse('apple')
-        self.assertFalse(t.error)
-        self.assertTrue(t.complete)
+        self.check(t, False, True)
 
         logger.debug('test 2')
         t = trie.Traverser(x)
         t.traverse('app')
-        self.assertFalse(t.error)
-        self.assertFalse(t.complete)
+        self.check(t, False, False)
         t.traverse('le')
-        self.assertFalse(t.error)
-        self.assertTrue(t.complete)
+        self.check(t, False, True)
 
         logger.debug('test 3')
         t = trie.Traverser(x)
         t.traverse('banana')
-        self.assertTrue(t.error)
-        self.assertFalse(t.complete)
+        self.check(t, True, False)
         t.traverse('cherry')
-        self.assertTrue(t.error)
-        self.assertFalse(t.complete)
+        self.check(t, True, False)
