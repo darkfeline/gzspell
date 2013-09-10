0b06633bb1c0ab55ef54b57610d741350c9cfba3
Allen Li <darkfeline@abagofapples.com>
2013-09-09 21:01:53 -0400

    Add more trie unit tests

--- gzspell/tests/test_trie.py
+++ gzspell/tests/test_trie.py
@@ -35,3 +35,33 @@ class TestTrie(unittest.TestCase):
         self.check(t, True, False)
         t.traverse('cherry')
         self.check(t, True, False)
+
+    def test_two(self):
+
+        x = trie.Trie()
+        x.add('apple')
+        x.add('app')
+
+        logger.debug('test 1')
+        t = trie.Traverser(x)
+        t.traverse('apple')
+        self.check(t, False, True)
+
+        logger.debug('test 2')
+        t = trie.Traverser(x)
+        t.traverse('app')
+        self.check(t, False, True)
+
+        logger.debug('test 3')
+        t = trie.Traverser(x)
+        t.traverse('app')
+        self.check(t, False, True)
+        t.traverse('le')
+        self.check(t, False, True)
+
+        logger.debug('test 4')
+        t = trie.Traverser(x)
+        t.traverse('nano')
+        self.check(t, True, False)
+        t.traverse('ha')
+        self.check(t, True, False)
