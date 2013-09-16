8aea1991c8a80944d5bcb5eafa51c4044767c876
Allen Li <darkfeline@abagofapples.com>
2013-09-16 01:52:18 -0400

    Add modified editdist() costs

--- gzspell/src/gzspell/analysis.py
+++ gzspell/src/gzspell/analysis.py
@@ -3,7 +3,74 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-cost = {'insert': 1, 'delete': 1, 'replace': 1}
+
+class Costs:
+
+    keys = 'qwertyuiopasdfghjklzxcvbnm-'
+
+    _neighbors = {
+        'q': ('w', 'a', 's'),
+        'w': ('q', 'a', 's', 'd', 'e'),
+        'e': ('w', 's', 'd', 'f', 'r'),
+        'r': ('e', 'd', 'f', 'g', 't'),
+        't': ('r', 'f', 'g', 'h', 'y'),
+        'y': ('t', 'g', 'h', 'j', 'u'),
+        'u': ('y', 'h', 'j', 'k', 'i'),
+        'i': ('u', 'j', 'k', 'l', 'o'),
+        'o': ('i', 'k', 'l', 'p'),
+        'p': ('o', 'l', '-'),
+        'a': ('q', 'w', 's', 'x', 'z'),
+        's': ('q', 'a', 'z', 'x', 'c', 'd', 'e', 'w'),
+        'd': ('w', 's', 'x', 'c', 'v', 'f', 'r', 'e'),
+        'f': ('e', 'd', 'c', 'v', 'b', 'g', 't', 'r'),
+        'g': ('r', 'f', 'v', 'b', 'h', 'y', 't'),
+        'h': ('t', 'g', 'b', 'n', 'j', 'u', 'y'),
+        'j': ('y', 'h', 'n', 'm', 'k', 'i', 'u'),
+        'k': ('u', 'j', 'm', 'l', 'o', 'i'),
+        'l': ('i', 'k', 'o', 'p'),
+        'z': ('a', 's', 'x'),
+        'x': ('z', 's', 'd', 'c'),
+        'c': ('x', 'd', 'f', 'v'),
+        'v': ('c', 'f', 'g', 'b'),
+        'b': ('v', 'g', 'h', 'n'),
+        'n': ('b', 'h', 'j', 'm'),
+        'm': ('n', 'j', 'k', 'l'),
+        '-': ('p'),
+    }
+
+    def __init__(self):
+        self.costs = [[None for i in len(self.keys)] for j in len(self.keys)]
+
+    def get(self, a, b):
+        return self.costs[self.keys.index(a)][self.keys.index(b)]
+
+    def set(self, a, b, v):
+        self.costs[self.keys.index(a)][self.keys.index(b)] = v
+
+    def compute(self):
+        for a in self._neighbors:
+            self._calc(a)
+
+    def _calc(self, a, val=0.5, delta=0.5):
+        try:
+            neighbors = self._neighbors[a]
+        except KeyError:
+            return
+        self.set(a, a, 0)
+        for k in neighbors:
+            old = self.get(a, k)
+            if old is None:
+                self.set(a, k, val)
+            else:
+                self.set(a, k, (val+old) / 2)
+            self._calc(k, val+delta, delta)
+
+costs = Costs()
+costs.compute()
+
+
+def repl_cost(a, b):
+    return costs.get(a, b)
 
 
 @lru_cache(1024)
@@ -41,24 +108,12 @@ def _editdist(word, target, i_word, i_target, table):
     if table[i_target][i_word] is None:
         table[i_target][i_word] = min(
             # insert in word
-            (
-                _editdist(word, target, i_word, i_target - 1, table) +
-                cost['insert']
-            ),
+            _editdist(word, target, i_word, i_target - 1, table) + 1,
             # delete in word
-            (
-                _editdist(word, target, i_word - 1, i_target, table) +
-                cost['delete']
-            ),
+            _editdist(word, target, i_word - 1, i_target, table) + 1,
             # replace, same
-            (
-                _editdist(word, target, i_word - 1, i_target - 1, table) +
-                (
-                    cost['replace'] if
-                    word[i_word - 1] != target[i_target - 1] else
-                    0
-                )
-            ),
+            _editdist(word, target, i_word - 1, i_target - 1, table) +
+            repl_cost(word[i_word - 1], target[i_target - 1])
         )
     logger.debug('Got %r', table[i_target][i_word])
     return table[i_target][i_word]
